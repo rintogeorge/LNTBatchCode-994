a174093add96895ba09b35048a6b2a2e05daaec7
rintogeorge <79743696+rintogeorge@users.noreply.github.com>
2022-03-01 15:27:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,9 @@ public static void main(String [] args){
 	System.out.println("Hello welcome ");
 int a=10;
 int b=20;
+int c=30;
 	System.out.println(a);
 	System.out.println(b);
+	System.out.println(c);
 }
 }
