e2c78488a59727bca34218536f1fc20e3652f1c0
rinto george <rintogeorge5@gmail.com>
2022-03-01 15:20:28 +0530

    4th commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld{
 public static void main(String [] args){
 	System.out.println("Hello welcome ");
 int a=10;
+int b=20;
 	System.out.println(a);
+	System.out.println(b);
 }
 }
